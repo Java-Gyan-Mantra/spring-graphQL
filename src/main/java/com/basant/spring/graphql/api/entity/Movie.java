8a326d1481e95f1279a87b82b97f45b708368ed8
Basant kumar Hota <basahota@users.noreply.github.com>
2018-01-07 15:56:57 +0530

    spring with graphql

---
+++ src/main/java/com/basant/spring/graphql/api/entity/Movie.java
@@ -0,0 +1,31 @@
+package com.basant.spring.graphql.api.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table
+@Setter
+@Getter
+@AllArgsConstructor
+@ToString
+public class Movie {
+	@Id
+	private String movieid;
+	private String movieName;
+	private String releaseDate;
+	private String[] actors;
+	private String director;
+	private String producer;
+
+	public Movie() {
+		super();
+	}
+
+}
